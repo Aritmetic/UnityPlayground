7008e9c381e9bb54834c6be0dbcdb96b34071457
Henry Timm <henry.timm@gmx.de>
2018-11-30 15:09:55 +0100

    particle effect is now affected by vertical axis

---
+++ Playground/Playground/Assets/ParticleEmitterTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEmitterTest : MonoBehaviour {
+
+	public ParticleSystem effect;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ParticleSystem.EmissionModule em = effect.emission;
+		float v = 70f + 70f * Input.GetAxis("Vertical");
+		em.rateOverTime = v;
+	}
+}
